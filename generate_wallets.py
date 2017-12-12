c3e38b4b7fce4af07ba8692fbec4f8369ba7ad58
hmallen <allenhm@gmail.com>
2017-12-12 03:40:36 -0500

    Fully functional integration of QR code creation and directory organization functions.

--- generate_wallets.py
+++ generate_wallets.py
@@ -1,30 +1,31 @@
 import argparse
 from bitcoin import *
 import csv
-#import datetime
+import datetime
 from electrum import mnemonic
 import logging
+from qrcodegen import QrCode, QrSegment
 import sys
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-output_dir = 'output/'
+dt_current = datetime.datetime.now().strftime('%m%d%Y_%H%M%S')
+output_dir = 'output/' + dt_current +'/'
 
-#dt_current = datetime.datetime.now().strftime('%m%d%Y-%H%M%S')
 #address_file = output_dir + dt_current + '_addresses.csv'
 address_file = output_dir + 'addresses.csv'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', '--number', default=1, type=int, help='Number of wallets to generate. [Default = 1]')
-parser.add_argument('-o', '--output', default=address_file, type=str, help='Specify output file. [Default = \'{datetime}_addresses.csv\'')
-parser.add_argument('-q', '--qr', default=False, help='Include QR codes of pub/priv keys as svg files in output.')
+parser.add_argument('-o', '--output', default=address_file, type=str, help='Specify output file. [Default = \'output/addresses.csv\'')
+parser.add_argument('-q', '--qr', action='store_true', default=False, help='Include QR codes of pub/priv keys as svg files in output.')
 args = parser.parse_args()
 
 wallet_count = args.number
 logger.debug('wallet_count: ' + str(wallet_count))
-output_file = args.output
-logger.debug('output_file: ' + output_file)
+output_path = args.output
+logger.debug('output_path: ' + output_path)
 qr_output = args.qr
 logger.debug('qr_output: ' + str(qr_output))
 
@@ -39,25 +40,78 @@ m = mnemonic.Mnemonic()
 
 def generate_wallets():
     wallet_list = []
-    for x in range(0, wallet_count):
-        seed = m.make_seed()
-        logger.debug(seed)
+    try:
+        for x in range(0, wallet_count):
+            seed = m.make_seed()
+            logger.debug(seed)
 
-        priv = sha256(seed)
-        logger.debug(priv)
-        
-        pub = privtopub(priv)
-        logger.debug(pub)
-        
-        addr_pub = pubtoaddr(pub)
-        logger.debug(addr_pub)
-        
-        addr_priv = privtoaddr(priv)
-        logger.debug(addr_priv)
+            priv = sha256(seed)
+            logger.debug(priv)
+            
+            pub = privtopub(priv)
+            logger.debug(pub)
+            
+            addr_pub = pubtoaddr(pub)
+            logger.debug(addr_pub)
+            
+            addr_priv = privtoaddr(priv)
+            logger.debug(addr_priv)
 
-        wallet_list.append([seed, addr_pub, addr_priv, pub, priv])
+            wallet_list.append([seed, addr_pub, addr_priv, pub, priv])
+            
+    except Exception as e:
+        logger.exception(e)
+        raise
 
     return wallet_list
+
+
+def create_svg(input_file):
+    global output_dir
+    
+    errcorlvl = QrCode.Ecc.QUARTILE # Error correction level. Need to determine best setting!
+    svg_list = []
+
+    with open(input_file, newline='') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+        try:
+            count = 0
+            output_dir_initial = output_dir
+            for row in csv_reader:
+                count += 1
+                output_dir = output_dir_initial + str(count) + '/'
+                os.makedirs(output_dir)
+                
+                qr_pub = QrCode.encode_text(row[1], errcorlvl)
+                qr_svg_pub = qr_pub.to_svg_str(4)
+                #logger.debug('PUBLIC')
+                #logger.debug(qr_svg_pub)
+                file_name_pub = output_dir + str(count) + '_pub.svg'
+                logger.debug(file_name_pub)
+                with open(file_name_pub, 'w') as svg_file:
+                    svg_file.write(qr_svg_pub)
+                
+                qr_priv = QrCode.encode_text(row[4], errcorlvl)
+                qr_svg_priv = qr_priv.to_svg_str(4)
+                #logger.debug('PRIVATE')
+                #logger.debug(qr_svg_priv)
+                file_name_priv = output_dir + str(count) + '_priv.svg'
+                logger.debug(file_name_priv)
+                with open(file_name_priv, 'w') as svg_file:
+                    svg_file.write(qr_svg_priv)
+
+                file_name_info = output_dir + str(count) + '_info.txt'
+                with open(file_name_info, 'w') as info_file:
+                    info_file.write(row[0])
+                    info_file.write('\n\n')
+                    info_file.write(row[1])
+                    info_file.write('\n\n')
+                    info_file.write(row[4])
+
+        except Exception as e:
+            logger.exception('Exception while creating svg files.')
+            logger.exception(e)
+            raise
         
 
 if __name__ == '__main__':
@@ -65,13 +119,15 @@ if __name__ == '__main__':
         wallets = generate_wallets()
         logger.debug(wallets)
 
-        with open(output_file, 'a', newline='') as csv_file:
+        with open(output_path, 'a', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             for x in range(0, len(wallets)):
                 csv_writer.writerow(wallets[x])
 
         if qr_output == True:
             logger.info('Creating QR codes.')
+            create_svg(output_path)
     
     except Exception as e:
         logger.exception(e)
+        sys.exit(1)
