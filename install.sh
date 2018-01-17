ce66224d374c900b163c7753f2ddbd74903fd193
hmallen <allenhm@gmail.com>
2018-01-17 17:43:54 -0500

    Additions to installer script.

--- install.sh
+++ install.sh
@@ -8,4 +8,5 @@ python3 -m virtualenv ~/.virtualenvs/electrum_wallet_generator &&
 source ~/.virtualenvs/electrum_wallet_generator/bin/activate &&
 pip install git+https://github.com/spesmilo/electrum.git &&
 pip install git+https://github.com/hmallen/DrawSVG.git &&
-pip install -r requirements.txt
+pip install -r requirements.txt &&
+#sed -i "s/check_www/\#check_www/g" ~/.virtualenvs/electrum_wallet_generator/
