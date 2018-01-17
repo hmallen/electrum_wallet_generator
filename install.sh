4fcb089068cee51a8832211c2b38069362d17332
hmallen <allenhm@gmail.com>
2018-01-17 17:22:38 -0500

    Working on install script. Not yet complete.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update &&
+sudo apt install python36 -y &&
+sudo python3 -m pip install virtualenv &&
+mkdir ~/.virtualenvs &&
+python3 -m virtualenv ~/.virtualenvs/electrum_wallet_generator &&
+source ~/.virtualenvs/electrum_wallet_generator/bin/activate &&
+pip install git+https://github.com/spesmilo/electrum.git &&
+pip install git+https://github.com/hmallen/DrawSVG.git &&
+pip install -r requirements.txt
